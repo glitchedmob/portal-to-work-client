1741f3cb18ad1b3dcf74ee9191963e48b9aecbef
Levi Zitting <me@levizitting.com>
2019-11-02 13:44:10 -0500

    WIP algolia search

--- src/pages/JobListing.vue
+++ src/pages/JobListing.vue
@@ -1,58 +1,51 @@
 <template>
-    <q-page id="listing-page">
-        <div class="listing-page-container">
-            <div class="search-bar" style="max-width: 300px">
-                <q-input square outlined v-model="text" placeholder="Search" value="">
-                    <q-btn flat class="search-button" icon="search"></q-btn>
-                </q-input>
+    <q-page-container>
+        <q-page id="listing-page">
+            <div class="listing-page-container">
+                <ais-instant-search :search-client="searchClient" index-name="jobs">
+                    <ais-search-box>
+                        <div class="search-bar" slot-scope="{ currentRefinement, isSearchStalled, refine }">
+                            <q-input
+                                square
+                                outlined
+                                :value="currentRefinement"
+                                @input="refine($event)"
+                                placeholder="Search"
+                            >
+                                <q-btn flat class="search-button" icon="search"></q-btn>
+                            </q-input>
+                        </div>
+                    </ais-search-box>
+                    <ais-hits>
+                        <job-card slot-scope="{ item }"></job-card>
+                    </ais-hits>
+                </ais-instant-search>
             </div>
-
-            <div class="card-container">
-
-                <job-card
-                    title="Delivery Driver"
-                    sub-title="Gold Mechanical"
-                    main-icon="favorite"
-                    walking-distance="5 min"
-                    busing-distance="10 min"
-                />
-
-                <job-card
-                    title="Delivery Driver"
-                    sub-title="Gold Mechanical"
-                    main-icon="favorite"
-                    walking-distance="5 min"
-                    busing-distance="10 min"
-                />
-
-                <job-card
-                    title="Delivery Driver"
-                    sub-title="Gold Mechanical"
-                    main-icon="favorite"
-                    walking-distance="5 min"
-                    busing-distance="10 min"
-                />
-
-                <job-card
-                    title="Delivery Driver"
-                    sub-title="Gold Mechanical"
-                    main-icon="favorite"
-                    walking-distance="5 min"
-                    busing-distance="10 min"
-                />
-            </div>
-        </div>
-    </q-page>
+        </q-page>
+    </q-page-container>
 </template>
 
 <script>
+    import { AisInstantSearch, AisSearchBox, AisHits } from 'vue-instantsearch';
+    import algoliasearch from 'algoliasearch/lite';
     import JobCard from "../components/JobCard";
 
     export default {
-        components: {JobCard},
-        data () {
-            return {
-                text: ''
+        components: {
+            JobCard,
+            AisInstantSearch,
+            AisSearchBox,
+            AisHits,
+        },
+        data: () => ({
+            searchClient: algoliasearch(
+                process.env.ALGOLIA_APP_ID,
+                process.env.ALGOLIA_SEARCH_KEY,
+            ),
+        }),
+        methods: {
+            test(args) {
+                console.log(args);
             }
         }
     }
@@ -80,7 +73,6 @@
     }
     .search-bar {
         width: 100%;
-        margin: 0 25px 25px 25px;
     }
     .card-container {
         width: 100%;
