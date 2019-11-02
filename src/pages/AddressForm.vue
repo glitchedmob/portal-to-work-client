34129940c66962740d072b389c193d6429de2705
Michael Copanos <michaelcopanos@Michaels-MacBook-Pro.local>
2019-11-01 23:36:33 -0500

    address page rough draft

--- src/pages/AddressForm.vue
+++ src/pages/AddressForm.vue
@@ -1,9 +1,32 @@
 <template>
-  <h1>Address form</h1>
+    <div class="wrapper">
+        <q-layout>
+            <q-page-container>
+                <q-page class="q-pt-md">
+                    <p><strong>Location For Job Search</strong></p>
+                    <q-btn class="q-px-xl q-pa-sm" size="md" unelevated rounded color="primary" label="Use My Current Location" />
+                    <h5>OR</h5>
+                    <q-form class="q-px-md q-pb-lg q-gutter-md">
+                        <q-input outlined v-model="text" label="Street Address" />
+                        <q-input outlined v-model="text" label="Line 2" />
+                        <q-input outlined v-model="text" label="City" />
+                        <q-input outlined v-model="text" label="state" />
+                        <q-input outlined v-model="text" label="Postal Code" />
+                    </q-form>
+                    <q-btn class="q-px-xl q-pa-sm" size="lg" unelevated rounded color="primary" label="Use This Address" />
+                </q-page>
+            </q-page-container>
+        </q-layout>
+
+    </div>
 </template>
 <script>
 
 </script>
-<style>
+<style scoped lang="sass">
+    .wrapper
+        text-align: center
 
+        p
+            font-size: 20px
 </style>
