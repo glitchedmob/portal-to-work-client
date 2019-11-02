5f64150acab6c3a5b3e91ab3a1391040200e5069
Michael Copanos <michaelcopanos@Michaels-MacBook-Pro.local>
2019-11-02 08:34:00 -0500

    title bar component

---
+++ src/components/TitleBarBackButton.vue
@@ -0,0 +1,23 @@
+<template>
+    <q-page-container class="row q-pt-md">
+        <div class="col-2">
+            <q-icon
+                color="primary"
+                size="md"
+                name="arrow_back"/>
+        </div>
+        <div class="col q-ml-xl">
+            <p class="text-h4 text-uppercase text-weight-medium">Job Details</p>
+        </div>
+    </q-page-container>
+</template>
+
+<script>
+    export default {
+        name: "TitleBarBackButton.vue"
+    }
+</script>
+
+<style scoped>
+
+</style>
