504626bf2c68d940e24851f4009e6c2f9de7383a
Michael Copanos <michaelcopanos@Michaels-MacBook-Pro.local>
2019-11-01 22:13:55 -0500

    pages and routes

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,10 +1,21 @@
 
 const routes = [
   {
-    path: '/',
-    component: () => import('layouts/MyLayout.vue'),
+    path:'/',
+    component: () => import('pages/AddressForm.vue'),
+  },
+  {
+    path: '/app',
+    component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/Index.vue') }
+      { path: 'jobs', component: () => import('pages/Index.vue') },
+      { path: 'jobs/detail', component: () => import('pages/JobDetail.vue') },
+      { path: 'notifications', component: () => import('pages/Notifications.vue') },
+      { path: 'saved-jobs', component: () => import('pages/SavedJobs.vue') },
+      { path: 'events', component: () => import('pages/Events.vue') },
+      { path: 'events/detail', component: () => import('pages/EventDetail.vue') },
+      { path: 'settings', component: () => import('pages/Settings.vue') }
+
     ]
   }
 ]
