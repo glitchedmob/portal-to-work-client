7d9192a2da8409a82fd4fedf9d367f837055faaf
Levi Zitting <me@levizitting.com>
2019-11-02 20:23:37 -0500

    Store user id

--- src/App.vue
+++ src/App.vue
@@ -5,13 +5,21 @@
 </template>
 
 <script>
+    import axios from 'axios';
+    import { mapMutations } from 'vuex';
     import { setup } from './common/onesignal';
 
     export default {
         name: 'App',
         beforeCreate() {
-            setup();
-            this.$store.commit('initialiseStore');
+            this.initialiseStore();
+
+            setup().then((userId) => {
+                this.updateUserId(userId);
+            });
+        },
+        methods: {
+            ...mapMutations(['initializeStore', 'updateUserId']),
         },
     };
 </script>
